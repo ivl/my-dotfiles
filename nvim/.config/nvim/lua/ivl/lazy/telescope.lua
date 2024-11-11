e998ffeb3542beb023ef2dcddf842032ddac2d6a
Ihor Vlasyuk <igor.vlasiuk@gmail.com>
2024-11-11 22:13:11 +0200

    added changes for telescope now all telescop actions <leader> + f<action>

--- nvim/.config/nvim/lua/ivl/lazy/telescope.lua
+++ nvim/.config/nvim/lua/ivl/lazy/telescope.lua
@@ -8,18 +8,24 @@ return {
         dependencies = { 'nvim-lua/plenary.nvim' },
         config = function()
             local builtin = require("telescope.builtin")
-            vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+            vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+            vim.keymap.set('n', '<leader>fk', builtin.keymaps, {})
             vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+            vim.keymap.set('n', '<leader>fw', builtin.grep_string, {})
+            vim.keymap.set('n', '<leader>ft', builtin.builtin, {})
             vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
             vim.keymap.set('n', '<leader>fm', builtin.marks, {})
+            vim.keymap.set('n', '<leader>fd', builtin.diagnostics, {})
+            vim.keymap.set('n', '<leader>fr', builtin.resume, {})
             vim.keymap.set('n', '<leader>fl', builtin.loclist, {})
             vim.keymap.set('n', '<leader>fq', builtin.quickfix, {})
+            vim.keymap.set('n', '<leader>f.', builtin.oldfiles, {})
             vim.keymap.set('n', '<leader>fr', builtin.registers, {})
             vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-            vim.keymap.set('n', '<leader>ps', function()
+            vim.keymap.set('n', '<leader>fs', function()
                 builtin.grep_string({ search = vim.fn.input("Grep > ") });
             end)
-            vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+            vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
         end
     }
 
