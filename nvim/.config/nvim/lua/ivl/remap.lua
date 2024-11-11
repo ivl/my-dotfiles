4cc2f8c0b7c1b2a0908912bbd1e7562749c6f69c
Ihor Vlasyuk <igor.vlasiuk@gmail.com>
2024-11-11 22:30:52 +0200

    added some changes to lsp

--- nvim/.config/nvim/lua/ivl/remap.lua
+++ nvim/.config/nvim/lua/ivl/remap.lua
@@ -31,6 +31,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
 		local opts = { buffer = ev.buf }
 		vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
 		vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+		vim.keymap.set("n", "rn", function() vim.lsp.buf.rename() end, opts)
 		vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
 		vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 		vim.keymap.set('n', '<leader>f', function()
